889d19f7a76667edd1debc7f68fe9e44fa5f17c7
Nathaniel Sandberg <nathaniel.sandberg@gmail.com>
2022-10-10 15:50:04 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestClientCredentials2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapGet("/HelloWorld", () =>
+{
+    return new string[] { "value", "someValue" };
+});
+
+app.MapPost("/token", async () =>
+{
+    var url = new Uri("https://identity-dev.fortellis.io/oauth2/aus1ni5i9n9WkzcYa2p7/v1/token");
+    var handler = new HttpClientHandler();
+    using var client = new HttpClient();
+    client.DefaultRequestHeaders.Add("Authorization", "Basic dXM4V0tWS3BLcnFHNW1lRHlxRDRKdUFzcktjY3ZHU0s6VW10WlYzR2pVODFibkk3SA==");
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    HttpContent data = new FormUrlEncodedContent(new[] 
+    {
+        new KeyValuePair<string, string>("scope", "anonymous"),
+        new KeyValuePair<string, string>("grant_type", "client_credentials")
+
+    });
+
+    Console.WriteLine($"This is what's in the data: {data.ToString}");
+
+    var res = await client.PostAsync(url, data);
+
+    var content = await res.Content.ReadAsStringAsync();
+
+    Console.WriteLine("This is the content of the response: " + content);
+
+    var tokenResponse = JsonConvert.DeserializeObject(content);
+    Console.WriteLine("This is the token response parsed: " + tokenResponse);
+    return content;
+});
+
+app.Run();
